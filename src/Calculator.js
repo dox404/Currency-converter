9cb63aa087aa1c3bbca51d407c6497092efe8c39
dox404 <muktarul.hoque@hotmail.com>
2024-08-11 23:20:51 +0530

    slight change

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Calculator() {
   return (
     <div>
-      <input placeholder="Currency"/>
-    </div>
-  )
+      <div class="flex">
+        <input type="text" id="name" name="name-input" className="slight-left" placeholder="First Name"/>
+          <input type="text" id="lastName" name="lastName-input" className="slight-left" placeholder="Last Name"/>
+          </div>
+      </div>
+      )
 }

9ba3219bf588e87061ddfe139c7abdb5e4fd5f7c
dox404 <muktarul.hoque@hotmail.com>
2024-08-17 23:27:35 +0530

    added dropdown bar

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,9 +4,14 @@ export default function Calculator() {
   return (
     <div>
       <div class="flex">
-        <input type="text" id="name" name="name-input" className="slight-left" placeholder="First Name"/>
-          <input type="text" id="lastName" name="lastName-input" className="slight-left" placeholder="Last Name"/>
-          </div>
+        <div class="dropdown-content">
+          <a href="#">Link 1</a>
+          <a href="#">Link 2</a>
+          <a href="#">Link 3</a>
+        </div>
+        <input type="text" id="name" name="name-input" className="slight-left" placeholder="First Name" />
+        <input type="text" id="lastName" name="lastName-input" className="slight-left" placeholder="Last Name" />
       </div>
-      )
+    </div>
+  )
 }
